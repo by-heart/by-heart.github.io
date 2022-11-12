66450fd2f66b04a88acf17160983ed3b3a98a4b9
anndgao <96456314+anndgao@users.noreply.github.com>
2022-11-11 19:29:36 -0500

    Update timer.js
    Making it so timer starts when button is clicked, not when page loads

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,17 +1,18 @@
 // Update later to make this so a timer is set for 5/10/appropriate interval
-const countDownDate = new Date().getTime() + 300000;
-var countdown  = function() {
+let countDownDate;
+
+let countdown = function() {
 
 	// Get the current date and time
-	var now = new Date().getTime();
+	let now = new Date().getTime();
 
 	// Find the distance between now and the count down date
-	var diff = countDownDate - now;
+	let diff = countDownDate - now;
 
 	// Time calculations for days, hours, minutes and seconds
-	var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+	let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+	let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
 	// Display the result in timer div
 	document.getElementById("timer").innerHTML = hours + "h "
@@ -22,5 +23,8 @@ var countdown  = function() {
    	document.getElementById("timer").innerHTML = "Time to start studying again!";
   }
 }
-// Update the count down every second
-var timer = setInterval(countdown, 1000);
+
+function startTimer() {
+	countDownDate = new Date().getTime() + 300000;
+	setInterval(countdown, 1000);
+}
